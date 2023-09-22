db49e9428b6fbc613698bb19d5713c01d9a76fd8
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-09-22 17:02:34 +0530

    public and private classes done

--- pureTS/dist/index.js
+++ pureTS/dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("Hello TS");
+// class User {
+//   public name: string
+//   private email: string
+//   readonly city: string = 'Deoli'
+//   constructor(name: string, email: string) {
+//     this.name = name
+//     this.email = email
+//   }
+// }
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.courseCount = 1;
+        this.city = 'Deoli';
+        this.name = name;
+        this.email = email;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get getCourseCount() {
+        return this.courseCount;
+    }
+    set setCourseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this.courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log('Token deleted');
+    }
+}
+const vaibhav = new User('vaibhav', 'vaibhav@v.c');
--- pureTS/src/index.ts
+++ pureTS/src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello TS")
+// class User {
+//   public name: string
+//   private email: string
+//   readonly city: string = 'Deoli'
+//   constructor(name: string, email: string) {
+//     this.name = name
+//     this.email = email
+//   }
+// }
+
+class User {
+  private courseCount: number = 1
+  readonly city: string = 'Deoli'
+  constructor(public name: string, private email: string) {
+    this.name = name
+    this.email = email
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`
+  }
+
+  get getCourseCount(): number {
+    return this.courseCount
+  }
+
+  set setCourseCount(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error('Course count should be more than 1')
+    }
+    this.courseCount = courseNum
+  }
+
+  private deleteToken() {
+    console.log('Token deleted')
+  }
+}
+
+const vaibhav = new User('vaibhav', 'vaibhav@v.c')
