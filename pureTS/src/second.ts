c845e745ce99954b5c960d9ed798caba69f69c4e
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-09-23 10:07:24 +0530

    importance of interfaces done

---
+++ pureTS/src/second.ts
@@ -0,0 +1,25 @@
+interface TakePhoto {
+  cameraMode: string
+  filter: string
+  burst: number
+}
+
+class Instagram implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {}
+}
+
+class Youtube implements TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+    public short: string
+  ) {}
+  createStory(): string {
+    return 'story created'
+  }
+}
